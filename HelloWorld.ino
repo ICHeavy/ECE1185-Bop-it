8dcdbf144f318e3a9111e47b5757142b2bc171d3
ICHeavy <102187487+ICHeavy@users.noreply.github.com>
2022-03-24 17:51:47 -0400

    Update HelloWorld.ino

--- HelloWorld.ino
+++ HelloWorld.ino
@@ -1,20 +1,3 @@
-#define outPin 13
-#define inPin 8
-
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(outPin, OUTPUT);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  if(digitalRead(inPin) == HIGH) {
-    digitalWrite(outPin, HIGH);
-    delay(1000);
-    digitalWrite(outPin, LOW);
-    delay(1000);
-  }
-}
 
 /*********
 One fish
